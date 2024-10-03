6b6e290db6dfaffa153d8339af53024c7499afd5
lieftsatyamsrivastava <satyamsrivastava384@gmail.com>
2024-10-04 03:49:52 +0530

    fixed the nav bar

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/Navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename= {import.meta.env.BASE_URL} >
+    <Navbar/>
       <App />
     </BrowserRouter>
   </StrictMode>,
